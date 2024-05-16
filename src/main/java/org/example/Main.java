2da19d004e69046a24200015f4568a318d222f52
Administrator <rados1234@wp.pl>
2024-05-16 16:16:18 +0200

    ww

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("33Hello world!");
+        System.out.println("33Hello world!Radek1");
     }
 }
